d269c0fe7520d6fa6626e12f065cce42ba98704a
Marco Fracassi <fracassi.marco@gmail.com>
2018-11-10 20:14:57 +0100

    Empty Kotlin Maven project

---
+++ src/main/kotlin/klyrics/Hello.kt
@@ -0,0 +1,6 @@
+package klyrics
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ src/test/kotlin/klyrics/HelloTest.kt
@@ -0,0 +1,8 @@
+package klyrics
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
